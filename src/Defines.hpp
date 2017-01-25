22557eae48071485ebee95054a050bcf06af8864
PoulainJulien <poulain-julien@hotmail.com>
2017-01-25 17:21:47 +0100

    Add file for some common things that may be useful

---
+++ src/Defines.hpp
@@ -0,0 +1,20 @@
+#ifndef UTILS_DEFINES_HPP
+#define UTILS_DEFINES_HPP
+
+#if defined _WIN32 || defined _WIN64
+    #define OS_WINDOWS
+    #define MakeDirectory(x) mkdir(x)
+    #define MakePause() system("pause")
+#else
+    #define OS_LINUX
+    #define MakeDirectory(x) mkdir(x, 0777)
+    #define MakePause() {printf("Appuyez sur entrée pour continuer... ");getchar();}
+#endif
+
+#ifndef PI
+#define PI 3.141592653589793238463
+#endif
+
+#endif
+
+
